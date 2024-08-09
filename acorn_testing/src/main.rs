781695aa62f5a54d89519d958ed8edc73dcfae7b
dbakaj <hbrahimaj@gmail.com>
2024-08-09 15:19:04 +0100

    Added an AcornApplication struct.

--- acorn_testing/src/main.rs
+++ acorn_testing/src/main.rs
@@ -1,15 +1,29 @@
 extern crate acorn;
 
-fn main() {
+struct AcornApplication {
+    m_window: acorn::core::Window
+}
 
-    let mut window = acorn::core::Window::new(640, 480, "Test");
+impl AcornApplication {
 
-    while !window.should_close() {
-        window.update();
+    pub fn new(width: u32, height: u32, title: &str) -> Self {
+        let window = acorn::core::Window::new(width, height, title);
+        Self { m_window: window }
+    }
 
-        unsafe {
-            gl::ClearColor(0.2, 0.3, 0.5, 1.0);
-            gl::Clear(gl::COLOR_BUFFER_BIT);
+    pub fn run(&mut self) {
+        while !self.m_window.should_close() {
+            self.m_window.update();
+    
+            unsafe {
+                gl::ClearColor(0.2, 0.3, 0.5, 1.0);
+                gl::Clear(gl::COLOR_BUFFER_BIT);
+            }
         }
     }
+}
+
+fn main() {
+    let mut app = AcornApplication::new(640, 480, "AcornApplication");
+    app.run();
 }
